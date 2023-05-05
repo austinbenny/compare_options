6b82c09fc7506326d2277f6d0102f3c353ca7810
austinbenny <austinbennyt@gmail.com>
2023-05-04 23:10:22 -0400

    moved to pkg wallstreet

--- compare_options.py
+++ compare_options.py
@@ -1,160 +1,105 @@
 # Author: Austin Benny
 
-import re
-import datetime
-import yfinance as yf
-import logger
+from datetime import datetime
+from textwrap import dedent
+
+from wallstreet import Call, Put
 
 
-class compareContracts:
-    def __init__(self, ticker, month, day, year, strike, cType):
-        self.ticker = ticker
-        self.strike = strike
-        self.day = day
-        self.month = month
-        self.year = year
-        self.cType = cType
+class CompareContracts:
+    def __init__(self, contract):
+        self.inp_contr = self.extract_specs(contract)
+        (
+            self.ticker,
+            self.expiry,
+            self.strike,
+            self.contract_type,
+        ) = self.inp_contr.values()
 
-        self.build()
+        option = self._get_contract()
+        metrics = self._calculate_metrics(option)
+        self._write_metrics(metrics)
 
-    def build(self):
-        self.GetOpt()
-        self.DoCalcs()
-        self.writeCSV()
-        self.write()
+    def _write_metrics(self, metrics):
+        # TODO: Use jinja
+        out_str = dedent(
+            f"""
+            __________________
+            {self.convert_to_informal_ref(self.inp_contr)} [Run on: {datetime.now()}]
+            __________________
+            {metrics['sample_perc_change']:.0%} move in ${self.ticker}          = {metrics['contr_perc_change']:.2%} move in option
+            1 day move in ${self.ticker}       = {metrics['theta_change']:.2%} decrease in option
+            {metrics['sample_perc_change']:.0%} IV move in ${self.ticker}       = {metrics['iv_change']:.2%} move in option
+            delta/premium            = {metrics['delta_prem']:.2f}
+            gamma/delta              = {metrics['gamma_del']:.2f}
+            extrinsicValue/theta     = {metrics['ext_theta']:.2f}
+            ask-bid spread           = {metrics['ask_bid_perc_diff']:.2%}
+            __________________
+            """
+        )
+
+        print(out_str)
+
+    def _calculate_metrics(self, option):
+        metrics = {}
+
+        metrics["ask_bid_perc_diff"] = float(
+            (option["ask"] - option["bid"]) / ((option["ask"] + option["bid"]) / 2)
+        )
+
+        if self.strike < option["underlying_price"]:
+            metrics["extrensicVal"] = (
+                option["premium"] - (option["underlying_price"] - self.strike)
+            )
+        else:
+            metrics["extrensicVal"] = option["premium"]
+
+        metrics["sample_perc_change"] = 0.01
+        metrics["delta_change"] = (
+            option["delta"] * option["underlying_price"] * metrics["sample_perc_change"]
+        )
+        metrics["contr_perc_change"] = metrics["delta_change"] / option["premium"]
+        metrics["iv_change"] = option["vega"] / option["premium"]
+        metrics["non_dim_iv"] = metrics["iv_change"] / option["iv"]
+        metrics["theta_change"] = option["theta"] / option["premium"]
+        metrics["break_even"] = (metrics["theta_change"] * option["premium"]) / (
+            option["underlying_price"] * option["delta"]
+        )
+        metrics["delta_prem"] = option["delta"] / option["premium"]
+        metrics["gamma_del"] = option["gamma"] / option["delta"]
+        metrics["ext_theta"] = option["extrensicVal"] / option["theta"]
+        # # indexes
+        # metrics["g_d_index"] = self.ratio_in_index(option["gamma"], option["delta"])
+        # metrics["d_p_index"] = self.ratio_in_index(option["delta"], option["premium"])
+        # metrics["e_t_index"] = self.ratio_in_index(option["extrensicVal"], option["theta"])
+        # metrics["non_dim_iv_index"] = self.ratio_in_index(metrics["iv_change"], option["iv"])
+
+        return metrics
+
+    def _get_contract(self):
+        if self.contract_type == "P":
+            ws_contr = Put(self.ticker, expiration=self.expiry)
+        else:
+            ws_contr = Call(self.ticker, expiration=self.expiry)
+
+        option = {}
+
+        option["premium"] = float(ws_contr.price)
+        option["underlying_price"] = float(ws_contr.underlying.price)
+        option["delta"] = float(ws_contr.delta())
+        option["theta"] = float(ws_contr.theta())
+        option["gamma"] = float(ws_contr.gamma())
+        option["vega"] = float(ws_contr.vega())
+        option["iv"] = float(ws_contr.implied_volatility())
+        option["bid"] = float(ws_contr.bid)
+        option["ask"] = float(ws_contr.ask)
+
+        return option
 
     @staticmethod
-    def ratioInIndex(var1, var2):
+    def ratio_in_index(var1, var2):
         """converts the ratios into an index between 0 and 100 kinda like the RSI"""
         return 100 - (100 / (1 + (var1 / var2)))
-
-    def writeCSV(self):
-        pass
-
-    def write(self):
-        x = datetime.datetime.now()
-        date = x.strftime("%x") + " " + x.strftime("%a")
-
-        # write to CSV
-
-        self.optStr = "\n__________________\n"
-        self.optStr += (
-            self.ticker
-            + " "
-            + str(self.month)
-            + "/"
-            + str(self.day)
-            + "/"
-            + str(self.year)
-            + " "
-            + str(self.strike)
-            + self.short
-            + " [Run on: "
-            + date
-            + "]"
-            + "\n"
-        )
-        self.optStr += "__________________\n"
-        self.optStr += "{:.0%} move in ${:12} = {:.2%} move in option\n".format(
-            self.percentChange, self.ticker, self.optionpercentChange
-        )
-        self.optStr += "1 day move in ${:9} = {:.2%} decrease in option\n".format(
-            self.ticker, self.thetaChange
-        )
-        self.optStr += "To earn money ${:9} > {:.2%} move per day\n".format(
-            self.ticker, self.breakEven
-        )
-        self.optStr += "{:.0%} IV move in ${:9} = {:.2%} move in option\n".format(
-            self.percentChange, self.ticker, self.ivChange
-        )
-        self.optStr += "delta/premium            = {:.2f} [index: {:.3f}]\n".format(
-            self.deltaPrem, self.d_p_index
-        )
-        self.optStr += "gamma/delta              = {:.2f} [index: {:.3f}]\n".format(
-            self.gammaDel, self.g_d_index
-        )
-        self.optStr += "extrinsicValue/theta     = {:.2f} [index: {:.3f}]\n".format(
-            self.extTheta, self.e_t_index
-        )
-        self.optStr += "premium per (vega/IV)    = {:.2f} [index: {:.3f}]\n".format(
-            self.nonDimIV, self.nonDimIV_index
-        )
-        self.optStr += "ask-bid spread           = {:.2f} [% diff: {:.0%}]\n".format(
-            self.ask - self.bid, self.percDiff
-        )
-        self.optStr += "premium                  = ${:.2f}\n".format(self.premium)
-        self.optStr += "__________________\n"
-
-        print(self.optStr)
-
-        with open("script_output/_compareOptionsLog.txt", "a") as log:
-            log.write(self.optStr)
-
-    def DoCalcs(self):
-        self.percentChange = 0.01
-        deltaChange = self.delta * self.price * self.percentChange
-        self.optionpercentChange = deltaChange / self.premium
-
-        self.ivChange = self.vega / self.premium
-        self.nonDimIV = self.ivChange / self.iv
-        self.thetaChange = self.theta / self.premium
-        self.breakEven = (self.thetaChange * self.premium) / (self.price * self.delta)
-        self.deltaPrem = self.delta / self.premium
-        self.gammaDel = self.gamma / self.delta
-        self.extTheta = self.extrensicVal / self.theta
-
-        # indexes
-        self.g_d_index = self.ratioInIndex(self.gamma, self.delta)
-        self.d_p_index = self.ratioInIndex(self.delta, self.premium)
-        self.e_t_index = self.ratioInIndex(self.extrensicVal, self.theta)
-        self.nonDimIV_index = self.ratioInIndex(self.ivChange, self.iv)
-
-    def GetOpt(self):
-        if self.cType == "Y":
-            self.cType = "Call"
-            self.short = "C"
-            try:
-                from wallstreet import Call as Type
-            except:
-                print("ERROR:Install wallstreet using pip install wallstreet")
-
-        elif self.cType == "N":
-            self.cType = "Put"
-            self.short = "P"
-            try:
-                from wallstreet import Put as Type
-            except:
-                print('ERROR:Install wallstreet using "pip install wallstreet"')
-
-        if len(str(self.year)) == 2:
-            self.year = int("20" + str(self.year))
-
-        # if (self.year != int(datetime.datetime.now().year)) or (self.year != int(datetime.datetime.now().year + 1)):
-        #     self.year = int(datetime.datetime.now().year)
-
-        print(type(self.ticker), self.day, self.month, self.year, self.strike)
-
-        try:
-            self.opt = Type(self.ticker, self.day, self.month, self.year, self.strike)
-        except:
-            print("ERROR:One or more options fundamentals is wrong")
-
-        self.premium = float(self.opt.price)
-        self.price = float(self.opt.underlying.price)
-        self.delta = float(self.opt.delta())
-        self.theta = float(self.opt.theta())
-        self.gamma = float(self.opt.gamma())
-        self.vega = float(self.opt.vega())
-        self.iv = float(self.opt.implied_volatility())
-        self.bid = float(self.opt.bid)
-        self.ask = float(self.opt.ask)
-
-        self.percDiff = float((self.ask - self.bid) / ((self.ask + self.bid) / 2))
-
-        if self.strike < self.price:
-            self.extrensicVal = self.premium - (self.price - self.strike)
-        else:
-            self.extrensicVal = self.premium
 
     @staticmethod
     def extract_specs(spec_str: str) -> dict(str):
@@ -162,7 +107,7 @@ class compareContracts:
         ticker = spec_list[0]
         expiry = spec_list[1]
         strike = spec_list[-1][:-1]
-        contract_type = spec_str[-1].upper()
+        contract_type = spec_str[-1]
 
         # Error check
         if not ticker.isalpha():
@@ -178,24 +123,22 @@ class compareContracts:
                 f"Incorrectly specified contract type [{contract_type}] in input string [{spec_str}]."
             )
         try:
-            datetime.datetime.strptime(expiry, "%M/%d/%y")
+            datetime.strptime(expiry, "%M-%d-%Y")
         except ValueError:
             raise ValueError(
-                f"Incorrect data format for date {expiry}, should be MM/DD/YY."
+                f"Incorrect data format for date {expiry}, should be MM-DD-YYYY."
             )
 
         return {
-            "ticker": ticker,
+            "ticker": ticker.upper(),
             "strike": strike,
-            "expiry": expiry,
-            "contract_type": contract_type,
+            "expiry": str(expiry),
+            "contract_type": contract_type.upper(),
         }
 
     @staticmethod
     def convert_to_contract_symbol(inp_dict: dict(str)) -> str:
-        expiry = datetime.datetime.strptime(inp_dict["expiry"], "%M/%d/%y").strftime(
-            "%y%M%d"
-        )
+        expiry = datetime.strptime(inp_dict["expiry"], "%M-%d-%Y").strftime("%y%M%d")
 
         return (
             f"{inp_dict['ticker']}"
@@ -203,6 +146,10 @@ class compareContracts:
             + f"{inp_dict['contract_type']}"
             + f"{1000 * float(inp_dict['strike']):08}"
         )
+
+    @staticmethod
+    def convert_to_informal_ref(inp_dict: dict(str)) -> str:
+        return f"{inp_dict['ticker']} {inp_dict['expiry']} {inp_dict['strike']}{inp_dict['contract_type']}"
 
 
 if __name__ == "__main__":
@@ -218,9 +165,9 @@ if __name__ == "__main__":
         help=(
             "Provide the contract specs in standard syntax. For example, "
             "a Call option for SPY at strike price $400 for expiration 06/16/2022 "
-            "should be input as `SPY 06/16/22 400C`"
+            "should be input as `SPY 06-16-2023 400C`"
         ),
     )
     args = parser.parse_args()
 
-    # compareContracts(ticker, month, day, year, strike, cType)
+    CompareContracts(args.contract)
